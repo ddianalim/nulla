1a57e254d8921e82756e27412f26bae7fed34501
dianalimnos <dianalim1117@gmail.com>
2018-10-26 10:00:08 -0700

    added login storyboard

--- Nulla/AppDelegate.swift
+++ Nulla/AppDelegate.swift
@@ -9,9 +9,6 @@
 import UIKit
 import Firebase
 
-//@UIApplicationMain
-//class AppDelegate: UIResponder, UIApplicationDelegate {
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
         

10e58021617eb65dc5cfa2440d61ea0b271cd484
dianalimnos <dianalim1117@gmail.com>
2018-10-26 10:08:09 -0700

    loginviewcontroller file

---
+++ Nulla/Controllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Nulla
+//
+//  Created by Diana Lim on 10/26/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+

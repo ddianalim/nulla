de5137af78a4c54550dfcba65fa76cb1397a66dd
dianalimnos <dianalim1117@gmail.com>
2018-10-26 22:02:18 -0700

    reinstalled podfile
    still error

--- Nulla/AppDelegate.swift
+++ Nulla/AppDelegate.swift
@@ -13,9 +13,12 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-                
-        // Override point for customization after application launch.
         FirebaseApp.configure()
+        let storyboard = UIStoryboard(name: "Login", bundle: .main)
+        if let initialViewController = storyboard.instantiateInitialViewController() {
+            window?.rootViewController = initialViewController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 

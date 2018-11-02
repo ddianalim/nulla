c25ed7520ba3c78e4a2c86debbae93f0c40c9bfb
dianalimnos <dianalim1117@gmail.com>
2018-11-01 23:29:10 -0700

    fixed bug!! it runs now!
    just had to remove and re add google info plist file

--- Nulla/AppDelegate.swift
+++ Nulla/AppDelegate.swift
@@ -7,13 +7,13 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        //FirebaseApp.configure()
+        FirebaseApp.configure()
         let storyboard = UIStoryboard(name: "Login", bundle: .main)
         if let initialViewController = storyboard.instantiateInitialViewController() {
             window?.rootViewController = initialViewController

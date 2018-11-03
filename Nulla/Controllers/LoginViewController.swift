14d02ba73695b2af5d391df741b88700c41273bc
dianalimnos <dianalim1117@gmail.com>
2018-11-02 21:50:03 -0700

    added simple error handling

--- Nulla/Controllers/LoginViewController.swift
+++ Nulla/Controllers/LoginViewController.swift
@@ -37,6 +37,11 @@ class LoginViewController: UIViewController {
 
 extension LoginViewController: FUIAuthDelegate {
     func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
         print("handle user signup / login")
     }
 }

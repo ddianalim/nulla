b8d8d621fab8e45f208acf0e728ec840f09ab699
dianalimnos <dianalim1117@gmail.com>
2018-11-05 08:37:04 -0800

    added outlets!

--- Nulla/Controllers/CreateUsernameViewController.swift
+++ Nulla/Controllers/CreateUsernameViewController.swift
@@ -11,11 +11,20 @@ import UIKit
 class CreateUsernameViewController: UIViewController {
     // MARK: - Subviews
     
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
     // MARK: - VC Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nextButton.layer.cornerRadius = 6
     }
     
     // MARK: - IBActions
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        // create new user in database
+    }
 }

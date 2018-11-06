63df171debc55eb4b3856f1716851844facab422
dianalimnos <dianalim1117@gmail.com>
2018-11-05 22:03:01 -0800

    updated nextButtonTapped to use service

--- Nulla/Controllers/CreateUsernameViewController.swift
+++ Nulla/Controllers/CreateUsernameViewController.swift
@@ -30,22 +30,11 @@ class CreateUsernameViewController: UIViewController {
         guard let firUser = Auth.auth().currentUser,
             let username = usernameTextField.text,
             !username.isEmpty else { return }
-
-        let userAttrs = ["username": username]
-
-        let ref = Database.database().reference().child("users").child(firUser.uid)
-
-        ref.setValue(userAttrs) { (error, ref) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
-                return
-            }
-
-            ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                let user = User(snapshot: snapshot)
-
-                // handle newly created user here
-            })
+        
+        UserService.create(firUser, username: username) { (user) in
+            guard let user = user else { return }
+            
+            print("Created new user: \(user.username)")
         }
     }
 }

3e1754b6f827f4aababfa62b12838f1303b8e57d
dianalimnos <dianalim1117@gmail.com>
2018-11-05 22:18:18 -0800

    update login

--- Nulla/Models/User.swift
+++ Nulla/Models/User.swift
@@ -31,4 +31,23 @@ class User {
         self.uid = snapshot.key
         self.username = username
     }
+    
+    // MARK: - Singleton
+    
+    private static var _current: User?
+    
+    static var current: User {
+
+        guard let currentUser = _current else {
+            fatalError("Error: current user doesn't exist")
+        }
+        
+        return currentUser
+    }
+    
+    // MARK: - Class Methods
+    
+    static func setCurrent(_ user: User) {
+        _current = user
+    }
 }

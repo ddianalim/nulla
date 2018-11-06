32120b255f88376daecbd129c74c17a604117c3f
dianalimnos <dianalim1117@gmail.com>
2018-11-05 22:31:59 -0800

    new constants.swift file

---
+++ Nulla/Supporting Files/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Nulla
+//
+//  Created by Diana Lim on 11/5/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    // ...
+}

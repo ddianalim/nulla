0ebd7491fedf73dd602964c0f8a80162cb15cb7b
dianalimnos <dianalim1117@gmail.com>
2018-11-05 22:42:31 -0800

    new Storyboard+Utility.swift file

---
+++ Nulla/Extensions/Storyboard+Utility.swift
@@ -0,0 +1,20 @@
+//
+//  Storyboard+Utility.swift
+//  Nulla
+//
+//  Created by Diana Lim on 11/5/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    enum MGType: String {
+        case main
+        case login
+        
+        var filename: String {
+            return rawValue.capitalized
+        }
+    }
+}

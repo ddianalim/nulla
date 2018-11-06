9c4c911bf981a9f9b27f9c33ad44be2237bf2376
dianalimnos <dianalim1117@gmail.com>
2018-11-05 22:00:34 -0800

    creates user on firebase

--- Nulla/Services/UserService.swift
+++ Nulla/Services/UserService.swift
@@ -7,7 +7,24 @@
 //
 
 import Foundation
+import FirebaseAuth.FIRUser
+import FirebaseDatabase
 
 struct UserService {
-    // insert user-related networking code here
+    static func create(_ firUser: FIRUser, username: String, completion: @escaping (User?) -> Void) {
+        let userAttrs = ["username": username]
+        
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(nil)
+            }
+            
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                let user = User(snapshot: snapshot)
+                completion(user)
+            })
+        }
+    }
 }

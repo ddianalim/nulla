05fe512fb018bdf36ff3b729e62812be0002d841
dianalimnos <dianalim1117@gmail.com>
2018-11-07 10:44:43 -0800

    updated configure initial root

--- Nulla/AppDelegate.swift
+++ Nulla/AppDelegate.swift
@@ -15,9 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
-        let initialViewController = UIStoryboard.initialViewController(for: .login)
-        window?.rootViewController = initialViewController
-        window?.makeKeyAndVisible()
+        configureInitialRootViewController(for: window)
         
         return true
     }

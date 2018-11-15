8ea0505494facef8344d121e8a5e444f27675174
dianalimnos <dianalim1117@gmail.com>
2018-11-14 22:36:41 -0800

    added presentImagePickerController(with:from:) method
    and cleaned up code

--- Nulla/Helpers/NullaPhotoHelper.swift
+++ Nulla/Helpers/NullaPhotoHelper.swift
@@ -17,21 +17,15 @@ class NullaPhotoHelper: NSObject {
     // MARK: - Helper Methods
     
     func presentActionSheet(from viewController: UIViewController) {
-        // 1
         let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .actionSheet)
         
-        // 2
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            // 3
             let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { action in
                 // do nothing yet...
             })
-            
-            // 4
             alertController.addAction(capturePhotoAction)
         }
         
-        // 5
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { action in
                 // do nothing yet...
@@ -40,11 +34,16 @@ class NullaPhotoHelper: NSObject {
             alertController.addAction(uploadAction)
         }
         
-        // 6
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alertController.addAction(cancelAction)
         
-        // 7
         viewController.present(alertController, animated: true)
+    }
+    
+    func presentImagePickerController(with sourceType: UIImagePickerControllerSourceType, from viewController: UIViewController) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = sourceType
+        
+        viewController.present(imagePickerController, animated: true)
     }
 }

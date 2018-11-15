b757e7f2e54123aa7dd8f663843d8c6537f2e898
dianalimnos <dianalim1117@gmail.com>
2018-11-14 22:25:59 -0800

    created NullaPhotoHelper

---
+++ Nulla/Helpers/NullaPhotoHelper.swift
@@ -0,0 +1,13 @@
+//
+//  NullaPhotoHelper.swift
+//  Nulla
+//
+//  Created by Diana Lim on 11/14/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+
+class NullaPhotoHelper: NSObject {
+
+}

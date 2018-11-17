7e8feaac9981c4eca8b79bd71d932e760971b104
dianalimnos <dianalim1117@gmail.com>
2018-11-17 09:10:04 -0800

    added new PostService file

---
+++ Nulla/Services/PostService.swift
@@ -0,0 +1,15 @@
+//
+//  PostService.swift
+//  Nulla
+//
+//  Created by Diana Lim on 11/17/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+import FirebaseStorage
+import FirebaseDatabase
+
+struct PostService {
+    
+}

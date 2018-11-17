87faf15444a6d5277d57a1c9596f82c50208d397
dianalimnos <dianalim1117@gmail.com>
2018-11-17 09:15:43 -0800

    added vars

--- Nulla/Models/Post.swift
+++ Nulla/Models/Post.swift
@@ -10,5 +10,14 @@ import UIKit
 import FirebaseDatabase.FIRDataSnapshot
 
 class Post {
-    // properties and initializers
+    var key: String?
+    let imageURL: String
+    let imageHeight: CGFloat
+    let creationDate: Date
+    
+    init(imageURL: String, imageHeight: CGFloat) {
+        self.imageURL = imageURL
+        self.imageHeight = imageHeight
+        self.creationDate = Date()
+    }
 }

8682628548ccaa109520c855913bbc5c700d5151
dianalimnos <dianalim1117@gmail.com>
2018-11-17 09:30:03 -0800

    calculates aspect height based off of image

---
+++ Nulla/Extensions/UIImage+Size.swift
@@ -0,0 +1,19 @@
+//
+//  UIImage+Size.swift
+//  Nulla
+//
+//  Created by Diana Lim on 11/17/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    var aspectHeight: CGFloat {
+        let heightRatio = size.height / 736
+        let widthRatio = size.width / 414
+        let aspectRatio = fmax(heightRatio, widthRatio)
+        
+        return size.height / aspectRatio
+    }
+}

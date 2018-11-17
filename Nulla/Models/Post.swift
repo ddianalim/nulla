f4584a6a66a373906990ffc236726cd3fd2ed024
dianalimnos <dianalim1117@gmail.com>
2018-11-17 09:14:18 -0800

    added new post.swift file

---
+++ Nulla/Models/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  Nulla
+//
+//  Created by Diana Lim on 11/17/18.
+//  Copyright © 2018 Diana Lim. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase.FIRDataSnapshot
+
+class Post {
+    // properties and initializers
+}
